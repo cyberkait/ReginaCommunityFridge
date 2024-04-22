c99191dbd9187bb2b8f745923d43c28aa417e7b4
Your Name <kaitlynn@Kaitlynns-MBP.lan>
2024-04-21 20:38:59 -0600

    Removed some unnecessary settings.

--- rcf_site/settings.py
+++ rcf_site/settings.py
@@ -138,14 +138,6 @@ STATIC_URL = "/static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 
-import dj_database_url
-
-if 'DATABASE_URL' in os.environ:
-    DATABASES['default'] = dj_database_url.config(
-        conn_max_age=500,
-        conn_health_checks=True,
-    )
-
 STORAGES = {
     # ...
     "staticfiles": {
